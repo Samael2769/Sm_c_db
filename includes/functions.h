6adecd7ac5e3cf11f2598271dfb029a536fc0ca3
Samael <jeremie.joannard@epitech.eu>
2024-10-01 17:49:49 +0200

    Adding buffer handling to parse commands
    [MOD] includes/functions.h, [MOD] includes/structs.h, [MOD] sources/input_handling.c, [MOD] sources/main.c,

--- includes/functions.h
+++ includes/functions.h
@@ -7,3 +7,6 @@ void print_prompt();
 void read_input(InputBuffer* input_buffer);
 InputBuffer* new_input_buffer();
 void close_input_buffer(InputBuffer* input_buffer);
+void execute_statement(Statement* statement);
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement);
+MetaCommandResult do_meta_command(InputBuffer* input_buffer);
--- includes/structs.h
+++ includes/structs.h
@@ -11,3 +11,22 @@ typedef struct {
   size_t buffer_length;
   ssize_t input_length;
 } InputBuffer;
+
+typedef enum {
+  META_COMMAND_SUCCESS,
+  META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum {
+  PREPARE_SUCCESS,
+  PREPARE_UNRECOGNIZED_STATEMENT
+} PrepareResult;
+
+typedef enum {
+  STATEMENT_INSERT,
+  STATEMENT_SELECT
+} StatementType;
+
+typedef struct {
+  StatementType type;
+} Statement;
