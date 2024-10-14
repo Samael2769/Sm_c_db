d3939951905b31b061120ee4bd6f2f43d98135b1
Samael <jeremie.joannard@epitech.eu>
2024-10-14 06:43:38 +0200

    Adding leaf splitting
    [MOD] includes/functions.h, [MOD] mydb.db, [MOD] sources/input_handling.c, [ADD] sm_db,

--- includes/functions.h
+++ includes/functions.h
@@ -20,6 +20,7 @@ void cursor_advance(Cursor* cursor);
 
 
 void initialize_leaf_node(void* node);
+void initialize_internal_node(void* node);
 uint32_t * leaf_node_num_cells(void* node);
 void* leaf_node_cell(void* node, uint32_t cell_num);
 uint32_t* leaf_node_key(void* node, uint32_t cell_num);
@@ -31,4 +32,16 @@ NodeType get_node_type(void* node);
 void set_node_type(void* node, NodeType type);
 
 void print_constants();
-void print_lead_node(void *node);
+void print_tree(Pager* pager, uint32_t page_num, uint32_t indentation_level);
+
+uint32_t get_unused_page_num(Pager* pager);
+void leaf_node_split_and_insert(Cursor* cursor, uint32_t key, Row* value);
+void create_new_root(Table* table, uint32_t right_child_page_num);
+uint32_t * internal_node_num_keys(void* node);
+uint32_t * internal_node_right_child(void* node);
+uint32_t * internal_node_cell(void* node, uint32_t cell_num);
+uint32_t * internal_node_child(void* node, uint32_t child_num);
+uint32_t * internal_node_key(void* node, uint32_t key_num);
+uint32_t get_node_max_key(void* node);
+bool is_node_root(void* node);
+void set_node_root(void* node, bool is_root);
